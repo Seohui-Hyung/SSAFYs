ddce4cb2b6d6d0f0401fc475d4b3606e7f6e4436
Seohui_Hyung <ro.opretzelcham@gmail.com>
2024-10-09 17:27:22 +0900

    🥺

--- Baekjoon/src/Main_Q_6550.java
+++ Baekjoon/src/Main_Q_6550.java
@@ -4,31 +4,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main_Q_6550 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		while(true) {
-			String str = br.readLine();
-			if(str == null) break;
-			st = new StringTokenizer(str, " ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-			String s = st.nextToken();
-			String t = st.nextToken();
-			
-			int cnt = 0; // 일치하는 글자 수
-			int now = 0; // 현재까지 검사한 글자 위치를 저장하기 위한 변수
-			
-			for(int i = 0; i < t.length(); i++) {
-				if(s.charAt(now) == t.charAt(i)) {
-					cnt++;
-					now++;
-					if(now == s.length()) break;
-				}
-			}
-			
-			if(cnt == s.length()) System.out.println("YES");
-			else System.out.println("NO");
-		}
-	}
+        String str;
+        while ((str = br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+
+            String s = st.nextToken();
+            String t = st.nextToken();
+
+            int now = 0; // 현재까지 검사한 글자 위치를 저장하기 위한 변수
+
+            for (int i = 0; i < t.length(); i++) {
+                if (now < s.length() && s.charAt(now) == t.charAt(i)) {
+                    now++;
+                    if (now == s.length()) break;
+                }
+            }
+
+            if (now == s.length()) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
